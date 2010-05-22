574465b2d93a0acc0c314048af471c880cfd7ac0
Jeff Green <jeffrey.n.green@gmail.com>
2010-05-22 18:58:40 -0400

    First draft of FloatTimeSeries.  Also added log4j.

---
+++ src/com/expensivedna/tulkas/FloatTimeSeries.java
@@ -0,0 +1,44 @@
+package com.expensivedna.tulkas;
+
+import gnu.trove.TFloatArrayList;
+import gnu.trove.TLongArrayList;
+
+/**
+ * @author jeffrey.n.green@gmail.com
+ */
+public class FloatTimeSeries {
+
+
+    private TFloatArrayList values;
+    private TLongArrayList times;
+
+    /**
+     * Creates a new, empty FloatTimeSeries.
+     */
+    public FloatTimeSeries() {
+        values = new TFloatArrayList();
+        times = new TLongArrayList();
+    }
+
+    /**
+     * Adds the (time, value) pair to this time series.  The time parameter must be greater than or equal to the largest
+     * time value previously added to this list (IE pairs must be added in chronological order).
+     *
+     * @param time  the time to be added.
+     * @param value the value to be added.
+     * @return true if the (time, value) pair was successfully added, false if not.
+     */
+    public boolean add(long time, float value) {
+        int size = times.size();
+        if (size > 0 && time > times.get(size - 1)) {
+            //log warning
+            return false;
+        }
+
+        times.add(time);
+        values.add(value);
+        return true;
+    }
+
+
+}
